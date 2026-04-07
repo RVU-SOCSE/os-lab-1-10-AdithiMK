f631f291451246ec35204136a418bb3ab84d1119
AdithiMK <adithimahendrankurapathbca25@rvu.edu.in>
2026-04-07 13:24:26 +0530

    prog 3 and 4

---
+++ prog3.sh
@@ -0,0 +1,25 @@
+echo "job1: Display date"
+date
+sleep 3
+
+echo "job2:displau current user"
+whoami
+sleep 3
+
+echo "job3: display calendar"
+cal
+sleep 2
+
+echo "job4: list the files"
+ls
+sleep 3
+
+echo "job5:display my system data"
+date +"%T"
+sleep 1
+
+echo "job6:print working directory"
+pwd
+
+echo "All jobs are finished"
+
